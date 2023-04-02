99e992ae127708eaa7aaf2b686f4c4368408d7a9
Mateo Mercado <matemerc@gmail.com>
2023-04-02 01:11:14 -0400

    Nineth commit. Solved 2.js...again

--- src/2.js
+++ src/2.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /* My File Edition */
 =======
 /***** My new Master File Edition *********/
 >>>>>>> db556e9 (Fourth commit. File 2.js edited)
+=======
+/* My Other Edition */
+>>>>>>> 84ad688 (Sixth commit. Created a new branch and edited 2.js)
