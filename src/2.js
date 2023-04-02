e7e8d3ede9c34d1891a9129b3eb15f5b4c041afb
Mateo Mercado <matemerc@gmail.com>
2023-04-02 01:35:13 -0400

    Thirtheenth commit. Now I'm sure this is the last one. File 2.js SOLVED

--- src/2.js
+++ src/2.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* My File Edition */
-=======
-/***** My new Master File Edition *********/
->>>>>>> db556e9 (Fourth commit. File 2.js edited)
-=======
 /* My Other Edition */
->>>>>>> 84ad688 (Sixth commit. Created a new branch and edited 2.js)
-=======
-/* My Other Edition */
->>>>>>> refs/remotes/origin/branchTwo
